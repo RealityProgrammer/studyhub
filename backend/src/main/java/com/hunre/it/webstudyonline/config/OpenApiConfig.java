a8b8c8a9e01fbc7b5a9b49433bd4136a4acd5e30
Feliss <109795744+Feliss-dev@users.noreply.github.com>
2025-10-02 16:40:13 +0700

    auth header

--- backend/src/main/java/com/hunre/it/webstudyonline/config/OpenApiConfig.java
+++ backend/src/main/java/com/hunre/it/webstudyonline/config/OpenApiConfig.java
@@ -3,17 +3,28 @@ package com.hunre.it.webstudyonline.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        SecurityScheme bearer = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("WebStudyOnline API")
-                        .version("v1")
-                        .description("API documentation for WebStudyOnline"));
+                .components(new Components().addSecuritySchemes(securitySchemeName, bearer))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .info(new Info().title("WebStudyOnline API").version("v1"));
     }
 }
