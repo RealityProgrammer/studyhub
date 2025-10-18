6f19925dabe02765fd212482a37086791d5630ec
Caxivitual <destinyhero789@gmail.com>
2025-10-18 20:05:03 +0700

    fix hazelcast error, remove EmailConfiguration, move to server properties instead

--- backend/src/main/java/com/hunre/it/webstudyonline/config/HazelcastConfig.java
+++ backend/src/main/java/com/hunre/it/webstudyonline/config/HazelcastConfig.java
@@ -1,7 +1,9 @@
 package com.hunre.it.webstudyonline.config;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,15 @@ public class HazelcastConfig {
     public HazelcastInstance hazelcastServerInstance() {
         Config config = new Config();
         config.getJetConfig().setEnabled(true);
+
+        NetworkConfig network = config.getNetworkConfig();
+        JoinConfig join = network.getJoin();
+
+        // Disable multicast
+        join.getMulticastConfig().setEnabled(false);
+
+        // Enable TCP/IP if needed
+        join.getTcpIpConfig().setEnabled(true).addMember("127.0.0.1");
 
         MapConfig mapConfig = new MapConfig("otpCodes")
                 .setTimeToLiveSeconds(60);
--- backend/src/main/java/com/hunre/it/webstudyonline/service/impl/IEmailServiceImpl.java
+++ backend/src/main/java/com/hunre/it/webstudyonline/service/impl/IEmailServiceImpl.java
@@ -4,8 +4,12 @@ package com.hunre.it.webstudyonline.service.impl;
 import com.hunre.it.webstudyonline.service.IEmailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +20,11 @@ public class IEmailServiceImpl implements IEmailService {
 
     @Override
     public void sendVerificationEmail(String to, String subject, String text) throws MessagingException {
+        Logger logger = LoggerFactory.getLogger(IEmailServiceImpl.class);
+
+        JavaMailSenderImpl impl = (JavaMailSenderImpl)emailSender;
+        logger.info("Email: \"{}\", Password: \"{}\"", impl.getUsername(), impl.getPassword());
+
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
@@ -24,6 +33,5 @@ public class IEmailServiceImpl implements IEmailService {
         helper.setText(text, true);
 
         emailSender.send(message);
-
     }
 }
