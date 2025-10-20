0c66fee7f0ca54c052bbb74f8c2a5d6a81eb5d84
Caxivitual <destinyhero789@gmail.com>
2025-10-20 22:18:58 +0700

    remove unused imports to make eslint shut up

--- frontends/admin/src/App.tsx
+++ frontends/admin/src/App.tsx
@@ -3,15 +3,8 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 
 import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
-import Chart from './pages/Chart';
 import ECommerce from './pages/Dashboard/ECommerce';
-import FormElements from './pages/Form/FormElements';
-import FormLayout from './pages/Form/FormLayout';
 import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Tables from './pages/Tables';
-import Alerts from './pages/UiElements/Alerts';
-import Buttons from './pages/UiElements/Buttons';
 import DefaultLayout from './layout/DefaultLayout';
 import CategoryList from './pages/Category/CategoryList.tsx';
 import CourseList from './pages/Course/CourseList.tsx';
